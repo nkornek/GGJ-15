ccf8552dcaa2c7c2a36c1c73415e5b6c6b1c5404
Nick <nkornek@gmail.com>
2015-01-25 13:03:32 -0500

    end of GGJ

--- GGJ 15/Assets/PointNclick.cs
+++ GGJ 15/Assets/PointNclick.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class PointNclick : MonoBehaviour {
 
+	public Camera currentCam;
+	public Animator cameraAnimator;
+
 	// Use this for initialization
 	void Start () {
+
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+
+		RaycastHit hit;
+
+		Ray ray = currentCam.ScreenPointToRay(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 100));
+
+		if (Input.GetMouseButtonDown(0) & Physics.Raycast (ray, out hit)) 
+		{
+			//clicked an object
+			cameraAnimator.SetTrigger(hit.transform.tag);
+		}
 	}
 }
